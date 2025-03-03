098a2aedfc58faa5bfca3a08e6f7355faa2e5555
nehmtech <nehmtech.ke@gmail.com>
2025-03-03 20:33:46 +0300

    My first commit

---
+++ mathutils/mathutils.py
@@ -0,0 +1,19 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a - b
+
+def divide(a, b):
+    return a / b
+
+def modulus(a, b):
+    return a % b
+
+def floor(a, b):
+    return a // b
+
+
